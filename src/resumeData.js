fa9e8f2db4a355aee50028ec85803e1a86f8176a
jimmyhuynh2000 <jimmyhuynh2000@vt.edu>
2022-11-01 16:43:06 -0400

    added project repository

---
+++ src/resumeData.js
@@ -0,0 +1,120 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/huangwai",
+    "name": "Jimmy Huynh",
+    "role": "Undergraduate CS Student @ Virginia Tech",
+    "linkedinId":" (571)388-6214 - jimmyhuynh2000@vt.edu",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Passionate student who's looking for more opportunities software development. Been exposed to many skills including UI Developement, Data Structures, Low-Level Programming, & much more.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jimmyhuynh-cs/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/huangwai",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.instagram.com/pixbyhuynh/",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Senior Undergraduate at Virginia Tech studying Computer Science. A hardworking and team-orientated member who is seeking to earn an internship where I can showcase my technical background to gather more experience towards my field.",
+    "address":"Herndon, Virginia",
+    "website":"(571)388-6214 jimmyhuynh2000@vt.edu",
+    "education":[
+      {
+        "UniversityName":"Virginia Tech",
+        "specialization":"Pursing BS in Computer Science",
+        "MonthOfPassing":"Aug 2019 -",
+        "YearOfPassing":"May 2023",
+        "Achievements":"Cumulative GPA: 3.30"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"GEOGRAPHIC INFORMATION SYSTEM (GIS) - JAVA",
+        "specialization":"RESPONSIBLE FOR DESIGNING A SYSTEM TO COLLECT AND EFFECTIVELY STORE AND SEARCH FOR GEOGRAPHIC FEATURES.",
+        "MonthOfLeaving":"March 2022",
+        "YearOfLeaving":"May 2022",
+        "Achievements":"• Produced a GIS parser application to parse command line argument files which outputted geographic records for 50+ command.\n" +
+            "• Managed data collection using a multi-integrated data structure database.\n" +
+            "• Retrieve 100+ daily records by matching specific key features by geographic coordinates, names, states.\n" +
+            "• Utilized hash table, bucket PR quadtree’s, and Buffer Pool as a front-end implementation to improve search speed."
+      },
+      {
+        "CompanyName":"LIFE LISTED APPLICATION - KOTLIN",
+        "specialization":"TOOK INITIATIVE TO REFORM AND CREATE A NEW MOBILE APPLICATION THAT MANAGES DAILY TO-DO TASKS INTO A SIMPLER UI.",
+        "MonthOfLeaving":"September 2021",
+        "YearOfLeaving":"December 2021",
+        "Achievements":"• Developed a mobile application called Lift Listed, a to-do management application that allowed users to write down tasks for the day and to make it much easier to use.\n" +
+            "• Learned to streamline the process and simulate an agile development environment.\n" +
+            "• Simplified the user interface, storyboard creator, and setting development goals to stay on track.\n" +
+            "• Implemented development practices such as maintaining documentation, GIT repository management, configuration management, and continuous deployment."
+      },
+        {
+            "CompanyName":"COVID-19 VISUALIZATION - JAVA",
+            "specialization":"DESIGNED A VISUALIZATION TOOL THAT MONITORS THE REAL-TIME SPREAD OF THE COVID-19 PANDEMIC IN AN AGILE DEVELOPMENT.",
+            "MonthOfLeaving":"April 2020",
+            "YearOfLeaving":"May 2020",
+            "Achievements":"• Showcased a mock readable visualization of what the user interface would look like.\n" +
+                "• Constructed appropriate Java objects to enhance and inherent parent classes.\n" +
+                "• Demonstrated proper documentation of classes and methods while running JUnit tests.\n" +
+                "• Devised a UML diagram to organize and visualize a system along with its main classes and objects."
+        },
+        {
+            "CompanyName":"WORK EXPERIENCE: FOOD LION",
+            "specialization":"CASHIER & GROCERY STOCKER",
+            "MonthOfLeaving":"March 2020",
+            "YearOfLeaving":"July 2020",
+            "Achievements":"• Contributed to store by managing their points of sale systems providing experience with professionalism and time management skills.\n" +
+                "• Acted as a point of contact between management and employees to optimize operations and deliver\n" +
+                "• Worked effectively with the team of multiple instances"
+        }
+    ],
+    "skillsDescription":"MAIN LANGUAGES",
+    "skills":[
+        {
+            "skillname":"Python | R | Java | C | C++ | Kotlin\n" +
+                "JSON | NodeJS | D3/P5 | SQL | MATLAB\n" +
+                "REST APIs | Server Deployment | PowerPoint | RStudio | Linux\n" +
+                "JavaScript | Agile Methodology | Git | React | HTML5/CSS"
+        },
+
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "interests":[
+      {
+        "description":""
+      },
+      {
+        "description":" "
+      }
+    ]
+  }
+  
+  export default resumeData
