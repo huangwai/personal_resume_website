6799ee74019e5cbbfb05a6c72d345700c64d99d0
jimmyhuynh2000 <jimmyhuynh2000@vt.edu>
2022-11-03 00:53:56 -0400

    added dates

--- src/resumeData.js
+++ src/resumeData.js
@@ -38,8 +38,8 @@ let resumeData = {
         {
             "CompanyName":"Fork-Join Framework (Multithreading) – C",
             "specialization":"ACHIEVED A SUCESSFUL IMPLEMENTATION OF A SMALL FORK AND JOIN FRAMEWORK THAT ALLOWS FOR PARALLEL EXECUTION OF PROGRAMS.",
-            "MonthOfLeaving":"October 2022",
-            "YearOfLeaving":"October 2022",
+            "MonthOfLeaving":"October 2022 - October 2022",
+            "YearOfLeaving":"",
             "Achievements":"•\tCreated a thread pool implementation that allows for dynamic task parallelism.\n" +
                 "•\tEnhanced efficiency by utilizing a work stealing thread pool approach to reduce contention of working tasks.\n" +
                 "•\tCorrectly utilized pthread mutexes to synchronize tasks and to allow for parallelism execution of tasks.\n" +
@@ -48,8 +48,8 @@ let resumeData = {
         {
             "CompanyName":"CUSH (BASH SHELL IMPLEMENTATION) - C",
             "specialization":"RECREATED THE BASH SHELL IN WHICH THE USER IS ABLE TO SEND COMMANDS TO START FOREGROUND & BACKGROUND PROCESSES.",
-            "MonthOfLeaving":"September 2022",
-            "YearOfLeaving":"September 2022",
+            "MonthOfLeaving":"September 2022 - September 2022",
+            "YearOfLeaving":"",
             "Achievements":"• Effectively worked in a team to collaborate a mock shell terminal implementation.\n" +
                 "• Reprised C to understand job processes and how background & foreground processes work." +
                 " • Implemented simple built-in commands (i.e., ‘jobs’, ‘fg’, ‘bg’, ‘kill’, ‘exit’, and ‘stop’).\n" +
@@ -58,8 +58,8 @@ let resumeData = {
       {
         "CompanyName":"GEOGRAPHIC INFORMATION SYSTEM (GIS) - JAVA",
         "specialization":"RESPONSIBLE FOR DESIGNING A SYSTEM TO COLLECT AND EFFECTIVELY STORE AND SEARCH FOR GEOGRAPHIC FEATURES.",
-        "MonthOfLeaving":"March 2022",
-        "YearOfLeaving":"May 2022",
+        "MonthOfLeaving":"March 2022 - April 2022",
+        "YearOfLeaving":"",
         "Achievements":"• Produced a GIS parser application to parse command line argument files which outputted geographic records for 50+ command.\n" +
             "• Managed data collection using a multi-integrated data structure database.\n" +
             "• Retrieve 100+ daily records by matching specific key features by geographic coordinates, names, states.\n" +
@@ -68,8 +68,8 @@ let resumeData = {
       {
         "CompanyName":"LIFE LISTED APPLICATION - KOTLIN",
         "specialization":"TOOK INITIATIVE TO REFORM AND CREATE A NEW MOBILE APPLICATION THAT MANAGES DAILY TO-DO TASKS INTO A SIMPLER UI.",
-        "MonthOfLeaving":"September 2021",
-        "YearOfLeaving":"December 2021",
+        "MonthOfLeaving":"September 2021 - December 2021",
+        "YearOfLeaving":"",
         "Achievements":"• Developed a mobile application called Lift Listed, a to-do management application that allowed users to write down tasks for the day and to make it much easier to use.\n" +
             "• Learned to streamline the process and simulate an agile development environment.\n" +
             "• Simplified the user interface, storyboard creator, and setting development goals to stay on track.\n" +
@@ -78,8 +78,8 @@ let resumeData = {
         {
             "CompanyName":"COVID-19 VISUALIZATION - JAVA",
             "specialization":"DESIGNED A VISUALIZATION TOOL THAT MONITORS THE REAL-TIME SPREAD OF THE COVID-19 PANDEMIC IN AN AGILE DEVELOPMENT.",
-            "MonthOfLeaving":"April 2020",
-            "YearOfLeaving":"May 2020",
+            "MonthOfLeaving":"April 2020 - May 2020",
+            "YearOfLeaving":"",
             "Achievements":"• Showcased a mock readable visualization of what the user interface would look like.\n" +
                 "• Constructed appropriate Java objects to enhance and inherent parent classes.\n" +
                 "• Demonstrated proper documentation of classes and methods while running JUnit tests.\n" +
@@ -88,8 +88,8 @@ let resumeData = {
         {
             "CompanyName":"WORK EXPERIENCE: FOOD LION",
             "specialization":"CASHIER & GROCERY STOCKER",
-            "MonthOfLeaving":"March 2020",
-            "YearOfLeaving":"July 2020",
+            "MonthOfLeaving":"March 2020 - July 2020",
+            "YearOfLeaving":"",
             "Achievements":"• Contributed to store by managing their points of sale systems providing experience with professionalism and time management skills.\n" +
                 "• Acted as a point of contact between management and employees to optimize operations and deliver\n" +
                 "• Worked effectively with the team of multiple instances"
